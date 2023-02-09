aae22d3b526a7b16cb787f5c5d64cbf8118000d0
@dmin <e.amaraa.3@live.com>
2023-02-09 17:03:38 +0800

    bilguun

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -88,7 +88,7 @@ export default function Layout({ children, title }: LayoutProps) {
         </header>
         <div>{children}</div>
         <footer className="flex justify-center items-center shadow-inner w-full h-10">
-          footer edited by bilguun zailaa
+          footer edited by BILGUUN
         </footer>
       </div>
     </>
